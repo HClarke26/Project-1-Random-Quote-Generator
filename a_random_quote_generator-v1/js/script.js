e3c21c08aff6ce9675a2b718766cb559d2c1ac08
HClarke26 <harryclarke26@gmail.com>
2022-02-07 13:43:40 +0000

    logged quotes to console

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -7,7 +7,7 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-// quotes array //
+// quotes array 
 
   const quotes = [
   
@@ -37,6 +37,7 @@ project 1 - A Random Quote Generator
    
  ];
 
+ // console.log(quotes);
 
 
 /***
