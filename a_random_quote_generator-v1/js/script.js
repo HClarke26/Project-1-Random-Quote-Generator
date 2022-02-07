711597de4c5f0cfb8197e9fd04a4a41729f60624
HClarke26 <harryclarke26@gmail.com>
2022-02-07 13:31:24 +0000

    quotes array
    quotes array added

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -7,9 +7,35 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+// quotes array //
+
+  const quotes = [
+  
+    { quote: "The greatest mistake you can make in life is to be continually fearing you will make one.", 
+      source: "Elbert Hubbard" },
+   
+    { quote: "The only way to do great work is to love what you do.", 
+      source: "Steve Jobs",
+      year: 2005 },
+   
+    { quote: "Don't criticize what you can't understand.",
+      source: "Bob Dylan",
+      citation: "The Times They Are A-Changin'" },
+   
+    { quote: "There are no traffic jams along the extra mile.", 
+     source: "Roger Staubach" },
+   
+    { quote: "Fall seven times and stand up eight.", 
+     source: "Japanese Proverb" },
+   
+    { quote: "You miss 100% of the shots you don't take.", 
+      source: "Wayne Gretzky",
+      tags: "Sport" },
+   
+    { quote: "The secret of getting ahead is getting started.", 
+      source: "Mark Twain" },
+   
+ ];
 
 
 
